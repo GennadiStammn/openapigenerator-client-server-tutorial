a70e7282edc13715322424df135ef64f62bd7aef
GennadiStammn <gennadi.stamm@googlemail.com>
2020-12-30 21:08:32 +0100

    frontend client testing

--- user-frontend/src/app/app.component.ts
+++ user-frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User, UserService } from 'openapi';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'user-frontend';
+
+  constructor(private readonly userService: UserService) { }
+
+  public getUser(): void {
+    this.userService
+      .getUser('get-test-id')
+      .subscribe((user) => {
+        console.log('Get user: ' + JSON.stringify(user));
+      });
+  }
+
+  public createUser(): void {
+    let createUser: User = {
+      id : 'create-test-id',
+      nickname: 'dummy-nickname' 
+    };
+
+    this.userService
+      .createUser(createUser)
+      .subscribe((user) => {
+        console.log('Create user: ' + JSON.stringify(user));
+      });
+  }
+
+  public updateUser(): void {
+    let createUser: User = {
+      id : 'update-test-id',
+      nickname: 'dummy-nickname' 
+    };
+
+    this.userService
+      .updateUser(createUser)
+      .subscribe((user) => {
+        console.log('Update user: ' + JSON.stringify(user));
+      });
+  }
+
+  public deleteUser(): void {
+    this.userService
+      .deleteUser('delete-test-id')
+      .subscribe((user) => {
+        console.log('Delete user: ' + JSON.stringify(user));
+      });
+  }
+  
 }
